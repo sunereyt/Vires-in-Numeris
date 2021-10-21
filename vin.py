c7a5ceca1d5cfcb5680aab1ab2e638591158c9ca
BillGatesIII <BillGatesIII@users.noreply.github.com>
2021-10-21 09:41:27 +0200

    clean up code

--- vin.py
+++ vin.py
@@ -19,6 +19,7 @@ class ViN(IStrategy):
     min_candle_vol: int = 0
     custom_buy_info = {}
     stoploss_count: int = 0
+    sideways_candles = 48
 
     minimal_roi = {"0": 100}
     stoploss = -0.99
@@ -29,7 +30,7 @@ class ViN(IStrategy):
     process_only_new_candles = True
     use_sell_signal = True
     sell_profit_only = False
-    startup_candle_count: int = 72
+    startup_candle_count: int = 18
 
     def populate_indicators(self, df: DataFrame, metadata: dict) -> DataFrame:
         if len(df) < self.startup_candle_count:
@@ -56,7 +57,7 @@ class ViN(IStrategy):
         df[f"mom_{i}"] = ta.MOM(df, timeperiod=i)
         up, mid, df[f"mom_{i}_low"] = ta.BBANDS(df[f"mom_{i}"], timeperiod=i, nbdevup=2.0, nbdevdn=2.0, matype=0)
 
-        ef = df[['close', 'uppertail', 'lowertail']].reset_index()
+        ef = df['close'].reset_index()
         for i in self.indicator_range:
             df[f"volume_{i}"] = df['volume'].rolling(window=i, min_periods=i).sum()
             df[f"close_corr_{i}"] = ef['index'].rolling(window=i, min_periods=i).corr(ef['close'], method='spearman')
@@ -84,13 +85,13 @@ class ViN(IStrategy):
         df_buy: DataFrame = df.loc[df.loc[:, 'buy'], ['date', 'buy_tag', 'buy']]
         for index, row in df_buy.iterrows():
             buy_date = row['date']
-            if buy_date not in self.custom_buy_info:
+            try:
+                self.custom_buy_info[buy_date]['buy_tags'] += row['buy_tag']
+                self.custom_buy_info[buy_date]['buy_signals'] += 1
+            except:
                 self.custom_buy_info[buy_date] = {}
                 self.custom_buy_info[buy_date]['buy_tags'] = row['buy_tag']
                 self.custom_buy_info[buy_date]['buy_signals'] = 1
-            else:
-                self.custom_buy_info[buy_date]['buy_tags'] += row['buy_tag']
-                self.custom_buy_info[buy_date]['buy_signals'] += 1
             self.custom_buy_info[buy_date][metadata['pair']] = row['buy_tag']
 
         return df
@@ -164,48 +165,45 @@ class ViN(IStrategy):
             sell = reduce(lambda x, y: x & y, sell_conditions)
             df.loc[sell, 'stop_tag'] = 'stop' + df['streak_min'].astype(str)
 
-        df.loc[:, 'sell'] = False
         return df
 
     def custom_sell(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
                     current_profit: float, **kwargs):
         df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        candle_1 = df.iloc[-1]
-        sell_tag, stop_tag, current_rate = candle_1['sell_tag'], candle_1['stop_tag'], candle_1['close']
-        if (sell_tag == '' and stop_tag == '') or candle_1['buy']:
-            return None
-
         if hasattr(trade, 'buy_tag') and trade.buy_tag is not None:
             buy_tag = trade.buy_tag
         trade_open_date = timeframe_to_prev_date(self.timeframe, trade.open_date_utc)
         df_trade: DataFrame = df[df['date'] >= trade_open_date]
         candles_between = df_trade.index[-1] - df_trade.index[0]
+        candle_1: Series = df_trade.iloc[-1].copy()
+        sell_tag, stop_tag, current_rate = candle_1['sell_tag'], candle_1['stop_tag'], candle_1['close']
         current_profit = (current_rate - trade.open_rate) / trade.open_rate
-
-        if current_profit < -0.04 and 'stop' in stop_tag:
+        sell_reason = None
+        if 'stop' in stop_tag and current_profit < -0.04:
             n = int("".join(filter(str.isdigit, stop_tag)))
             if n <= candles_between:
                 self.stoploss_count += 1
-                log.info(f"custom_sell: stop # {self.stoploss_count} for pair {pair} with loss {round(current_profit, 2)}, stop_tag {stop_tag} on candle {candle_1['date']}.")
-                return f"{stop_tag} ({buy_tag})"
-
-        close_diff = df_trade['close'].max() / df_trade['close'].min()
-        mfi = mfi_enh(df, length=candles_between)
-        # cmf = cmf_enh(df, length=candles_between)
-        if current_profit <= 0.015 and 'sell' in sell_tag and len(df_trade) >= self.startup_candle_count and close_diff <= 1.03 and mfi.iat[-1] > 50:
+                log.info(f"custom_sell: stop # {self.stoploss_count} for pair {pair} with loss {round(current_profit, 2)} and stop_tag {stop_tag} on candle {candle_1['date']}.")
+                sell_reason = f"{stop_tag} ({buy_tag})"
+        elif 'sell' in sell_tag:
             n = int("".join(filter(str.isdigit, sell_tag)))
             if n <= candles_between:
-                return f"sideways_{sell_tag} ({buy_tag})"
+                close_diff = df_trade['close'].max() / df_trade['close'].min()
+                mfi = mfi_enh(df, length=candles_between)
+                # cmf = cmf_enh(df, length=candles_between)
+                if current_profit <= 0.015:
+                    if len(df_trade) >= self.sideways_candles and close_diff <= 1.03 and mfi.iat[-1] > 50:
+                        sell_reason = f"sideways_{sell_tag} ({buy_tag})"
+                else:
+                    if 'sell+' in sell_tag and mfi.iat[-1] > 50:
+                        sell_reason = f"{sell_tag} ({buy_tag})"
+                    elif 'sell-' in sell_tag and mfi.iat[-1] > 18:
+                        sell_reason = f"{sell_tag} ({buy_tag})"
+                if sell_reason is not None:
+                    log.info(f"custom_sell: sell for pair {pair} with profit/loss {round(current_profit, 2)} and sell_tag {sell_tag} on candle {candle_1['date']}.")
 
-        if current_profit > 0.015 and 'sell' in sell_tag:
-            n = int("".join(filter(str.isdigit, sell_tag)))
-            if n <= candles_between:
-                if 'sell+' in sell_tag and mfi.iat[-1] > 50:
-                    return f"{sell_tag} ({buy_tag})"
-                elif 'sell-' in sell_tag and mfi.iat[-1] > 18:
-                    return f"{sell_tag} ({buy_tag})"
-
-        return None
+        candle_1['sell'] = sell_reason is not None
+        return sell_reason
 
     def bot_loop_start(self, **kwargs) -> None:
         self.min_candle_vol = self.config['stake_amount']
@@ -217,7 +215,6 @@ class ViN(IStrategy):
         df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         candle_1: Series = df.iloc[-1]
         buy_candle_date = candle_1['date']
-
         if buy_candle_date in self.custom_buy_info.keys():
             pairs = len(self.dp.current_whitelist())
             max_concurrent_buy_signals = int(pairs * 0.08)
