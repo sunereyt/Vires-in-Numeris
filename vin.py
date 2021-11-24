f69879310290d0e5820f15ecfcd698aabb575914
BillGatesIII <BillGatesIII@users.noreply.github.com>
2021-11-24 19:28:51 +0100

    stricter buys, higher total profit

--- vin.py
+++ vin.py
@@ -36,7 +36,7 @@ class ViN(IStrategy):
         return [
             {
                 "method": "CooldownPeriod",
-                "stop_duration_candles": 54
+                "stop_duration_candles": 36
             }
         ]
 
@@ -108,8 +108,7 @@ class ViN(IStrategy):
     def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float,
                             time_in_force: str, current_time: datetime, **kwargs) -> bool:
         df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        candle_1 = df.iloc[-1]
-        buy_candle_date = candle_1['date']
+        buy_candle_date = df['date'].iloc[-1]
         d = buy_candle_date.strftime('%Y-%m-%d %H:%M')
         try:
             buy_info = self.custom_buy_info[buy_candle_date]
@@ -117,7 +116,7 @@ class ViN(IStrategy):
             buy_signal_count = buy_info['buy_signals']
             if self.max_concurrent_buy_signals_check:
                 pairs = len(self.dp.current_whitelist())
-                max_concurrent_buy_signals = max(int(pairs * 0.08), 18)
+                max_concurrent_buy_signals = max(int(pairs * 0.08), 8)
                 if buy_signal_count > max_concurrent_buy_signals:
                     log.info(f"{d} confirm_trade_entry: Cancel buy for pair {pair} with buy tag {buy_tags}. There are {buy_signal_count} concurrent buy signals (max = {max_concurrent_buy_signals}).")
                     return False
@@ -129,6 +128,20 @@ class ViN(IStrategy):
 
     def confirm_trade_exit(self, pair: str, trade: "Trade", order_type: str, amount: float,
                            rate: float, time_in_force: str, sell_reason: str, **kwargs) -> bool:
+        df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+        buy_candle_date = df['date'].iloc[-1]
+        d = buy_candle_date.strftime('%Y-%m-%d %H:%M')
+        try:
+            buy_info = self.custom_buy_info[buy_candle_date]
+            buy_signal_count = buy_info['buy_signals']
+            if self.max_concurrent_buy_signals_check:
+                pairs = len(self.dp.current_whitelist())
+                max_concurrent_buy_signals = max(int(pairs * 0.04), 4)
+                if buy_signal_count > max_concurrent_buy_signals:
+                    log.info(f"{d} confirm_trade_exit: Cancel sell for pair {pair}. There are {buy_signal_count} concurrent buy signals (max = {max_concurrent_buy_signals}).")
+                    return False
+        except:
+            return True
         return True
 
 def vwrs(df: DataFrame, length: int) -> Series:
@@ -143,8 +156,8 @@ class ViNBuyPct(ViN):
     def populate_indicators_buy(self, df: DataFrame, metadata: dict) -> DataFrame:
         df_closechange = df['close'] - df['close'].shift(1)
         i = 12
-        df['updown'] = np.where(df_closechange.rolling(window=i, min_periods=2).sum().gt(0), 1, np.where(df_closechange.rolling(window=i, min_periods=2).sum().lt(0), -1, 0))
-        df[f"streak_b"] = df['updown'].groupby((df['updown'].ne(df['updown'].shift(1))).cumsum()).cumsum()
+        df['updown'] = np.where(df_closechange.rolling(window=i, min_periods=i).sum().gt(0), 1, np.where(df_closechange.rolling(window=i, min_periods=i).sum().lt(0), -1, 0))
+        df[f"streak_h"] = df['updown'].groupby((df['updown'].ne(df['updown'].shift(1))).cumsum()).cumsum()
         df.drop(columns=['updown'], inplace=True)
         for i in self.buy_lookback_range:
             df[f"pctchange_{i}"] = df['close'].pct_change(periods=i)
@@ -157,22 +170,24 @@ class ViNBuyPct(ViN):
 
     def populate_buy_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
         df.loc[:, 'buy_tag'] = ''
-        df.loc[:, 'buy_tags_sum'] = 0
+        df.loc[:, 'buy_tag_sum'] = 0
         for i in self.buy_lookback_range:
             buy_conditions = [
                 df[f"candle_count_{self.startup_candle_count}"].ge(self.startup_candle_count),
-                df['volume'].ge(self.min_candle_vol),
+                df['volume'].ge(self.min_candle_vol * 1.8),
                 df['streak_s_min'].le(-1),
+                df['streak_s_max'].between(-5, 0),
+                df['streak_h'].ge(-19),
                 df['streak_s_min_change'].le(0.98),
-                df['streak_s_min'].ge(df['streak_b']),
+                df['streak_s_min'].ge(df['streak_h']),
                 (df[f"pctchange_{i}"] / df[f"bb_pctchange_{i}_lo"]).between(1.01, 1.39),
                 (df[f"bb_pctchange_{i}_up"] - df[f"bb_pctchange_{i}_lo"]).ge(0.02),
-                (df['lc2_adj'] / df['close']).le(0.995)
+                (df['lc2_adj'] / df['close']).between(0.975, 0.995)
             ]
             buy = reduce(lambda x, y: x & y, buy_conditions)
             df.loc[buy, 'buy_tag'] += f"{i} "
-            df.loc[buy, 'buy_tags_sum'] += i
-        df.loc[:, 'buy'] = df['buy_tags_sum'].ge(25)
+            df.loc[buy, 'buy_tag_sum'] += i
+        df.loc[:, 'buy'] = df['buy_tag_sum'].ge(25)
         df.loc[:, 'buy_tag'] = df['buy_tag'].str.strip()
         self.fill_custom_buy_info(df, metadata)
         return df
@@ -189,10 +204,10 @@ class ViNSellCorr(ViN):
         candle_1 = df_trade.iloc[-1]
         d = candle_1['date'].strftime('%Y-%m-%d %H:%M')
         current_profit = (candle_1['close'] - trade.open_rate) / trade.open_rate
-        if trade_len <= 2 or candle_1['buy'] or candle_1['streak_b'] >= candle_1['streak_s_min'] or (trade_len < self.lookback_candles and -0.04 < current_profit < 0.04) or abs(candle_1['streak_s_min']) <= 1:
+        if trade_len <= 2 or candle_1['buy'] or (trade_len < self.lookback_candles and -0.04 < current_profit < 0.04) or candle_1['streak_h'] >= candle_1['streak_s_min'] or abs(candle_1['streak_s_min']) <= 1:
             return None
         t = 'profit' if current_profit > 0 else 'loss'
-        i = min(trade_len, self.lookback_candles + int(current_profit * 18))
+        i = min(trade_len, self.lookback_candles - int(current_profit * 18))
         j = i // 2
         ef = df_trade[['close', 'hlc3_adj', 'volume']].reset_index()
         ef['vwrs_sell'] = vwrs(ef, length=i)
